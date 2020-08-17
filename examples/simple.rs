21f507ad26a52e795e091f57e4bcdaf156acf67a
Kyle Burke <kyle.n.burke@gmail.com>
2020-08-17 13:29:52 -0500

    Cleanup simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,30 +1,25 @@
 use engine::{
 	vulkan::{Context, Renderer},
-	mesh::{self, Mesh},
+	mesh::{Mesh, Material},
 	geometry3d,
 	Object3D,
 	Camera,
 	lights::AmbientLight
 };
-
-const WINDOW_WIDTH: u32 = 1280;
-const WINDOW_HEIGHT: u32 = 720;
-
 fn main() {
 	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 	glfw.window_hint(glfw::WindowHint::ClientApi(glfw::ClientApiHint::NoApi));
-	let (mut window, events) = glfw.create_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Simple", glfw::WindowMode::Windowed).unwrap();
+	let (mut window, events) = glfw.create_window(1280, 720, "Simple", glfw::WindowMode::Windowed).unwrap();
 	window.set_key_polling(true);
 	window.set_framebuffer_size_polling(true);
 
 	let context = Context::new(&glfw, &window);
-	let (width, height) = window.get_framebuffer_size();
-	let mut renderer = Renderer::new(&context, width as u32, height as u32);
+	let (framebuffer_width, framebuffer_height) = window.get_framebuffer_size();
+	let mut renderer = Renderer::new(&context, framebuffer_width as u32, framebuffer_height as u32);
 
-	let dynamic_box = Mesh::new(Box::new(geometry3d::Box {}), mesh::Material::Basic);
-	let mut dynamic_meshes = vec![dynamic_box];
+	let mut meshes = [Mesh::new(Box::new(geometry3d::Box {}), Material::Basic)];
 
-	let mut camera = Camera::new(WINDOW_WIDTH as f32 / WINDOW_HEIGHT as f32, 75.0, 0.1, 10.0);
+	let mut camera = Camera::new(framebuffer_width as f32 / framebuffer_height as f32, 75.0, 0.1, 10.0);
 	camera.translate_z(-2.0);
 
 	let ambient_light = AmbientLight::new();
@@ -45,19 +40,19 @@ fn main() {
 			}
 		}
 
-		let (width, height) = window.get_framebuffer_size();
-		if width == 0 || height == 0 {
+		let (framebuffer_width, framebuffer_height) = window.get_framebuffer_size();
+		if framebuffer_width == 0 || framebuffer_height == 0 {
 			glfw.wait_events();
 			continue;
 		}
 
 		if framebuffer_resized {
-			renderer.recreate_swapchain(width as u32, height as u32);
-			camera.projection_matrix.make_perspective(width as f32 / height as f32, 75.0, 0.1, 10.0);
+			renderer.recreate_swapchain(framebuffer_width as u32, framebuffer_height as u32);
+			camera.projection_matrix.make_perspective(framebuffer_width as f32 / framebuffer_height as f32, 75.0, 0.1, 10.0);
 		}
 		
-		dynamic_meshes[0].rotate_y(0.0001);
+		meshes[0].rotate_y(0.0001);
 
-		renderer.render(&window, &mut camera, &mut dynamic_meshes, &ambient_light, &[], &[]);
+		renderer.render(&window, &mut camera, &mut meshes, &ambient_light, &[], &[]);
 	}
 }
