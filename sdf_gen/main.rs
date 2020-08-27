7737e6c165a12fa5ac7691a2a0fcca93184886da
Kyle Burke <kyle.n.burke@gmail.com>
2020-08-27 15:03:11 -0500

    Simple font distance field implementation

--- sdf_gen/main.rs
+++ sdf_gen/main.rs
@@ -26,10 +26,12 @@ fn main() {
 	let output_bmp_file_path = if let Some(i) = output_bmp_file_path_index { Some(&args[i + 1]) } else { None };
 
 	let (mut glyphs, space_advance) = rasterize_font(input_font_file_path, font_size);
-	let atlas = create_atlas(&mut glyphs);
+	let spread = 4;
+	let atlas = create_atlas(&mut glyphs, spread);
+	let field = create_distance_field(&atlas, spread as f32);
 
 	if let Some(file_path) = output_bmp_file_path {
-		save_to_bitmap(file_path, &atlas);
+		save_to_bitmap(file_path, &field);
 	}
 
 	save_to_font_file(output_font_file_path, &mut glyphs, space_advance, &atlas);
@@ -88,7 +90,7 @@ fn rasterize_font(file_path: &str, font_size: u32) -> (Vec<Glyph>, i32) {
 	(glyphs, space_advance)
 }
 
-fn create_atlas(glyphs: &mut Vec<Glyph>) -> Vec<Vec<u8>> {
+fn create_atlas(glyphs: &mut Vec<Glyph>, spread: usize) -> Vec<Vec<u8>> {
 	glyphs.sort_unstable_by(|a, b| (b.size.0 * b.size.1).cmp(&(a.size.0 * a.size.1)));
 	let mut atlas: Vec<Vec<u8>> = Vec::new();
 
@@ -96,8 +98,8 @@ fn create_atlas(glyphs: &mut Vec<Glyph>) -> Vec<Vec<u8>> {
 		let atlas_height = atlas.len();
 		let atlas_width = if atlas_height == 0 { 0 } else { atlas[0].len() };
 
-		let glyph_width = glyph.size.0 as usize;
-		let glyph_height = glyph.size.1 as usize;
+		let glyph_width = glyph.size.0 as usize + spread * 2;
+		let glyph_height = glyph.size.1 as usize + spread * 2;
 
 		let atlas_col_bound = atlas_width.saturating_sub(glyph_width - 1);
 		let atlas_row_bound = atlas_height.saturating_sub(glyph_height - 1);
@@ -117,7 +119,7 @@ fn create_atlas(glyphs: &mut Vec<Glyph>) -> Vec<Vec<u8>> {
 				}
 
 				// Glyph can fit here
-				place_glyph(&mut atlas, atlas_row_index, atlas_col_index, glyph);
+				place_glyph(&mut atlas, atlas_row_index, atlas_col_index, glyph, spread);
 				continue 'glyph_loop;
 			}
 		}
@@ -142,21 +144,34 @@ fn create_atlas(glyphs: &mut Vec<Glyph>) -> Vec<Vec<u8>> {
 		}
 
 		expand_atlas(&mut atlas, vertical_expansion, horizontal_expansion);
-		place_glyph(&mut atlas, pos_row, pos_col, glyph);
+		place_glyph(&mut atlas, pos_row, pos_col, glyph, spread);
 	}
 
 	atlas
 }
 
-fn place_glyph(atlas: &mut Vec<Vec<u8>>, atlas_row: usize, atlas_col: usize, glyph: &mut Glyph) {
+fn place_glyph(atlas: &mut Vec<Vec<u8>>, atlas_row: usize, atlas_col: usize, glyph: &mut Glyph, spread: usize) {
+	let glyph_width = glyph.size.0;
+	let glyph_height = glyph.size.1;
+
+	// Zero out the whole region including the padding
+	for row in 0..glyph_height as usize + 2 * spread {
+		for col in 0..glyph_width as usize + 2 * spread {
+			atlas[atlas_row + row][atlas_col + col] = 0;
+		}
+	}
+
+	// Place the glyph in the non padded region
 	let glyph_pitch_abs = glyph.pitch.abs() as usize;
 
 	for glyph_row in 0..glyph.size.1 as usize {
 		for glyph_col in 0..glyph.size.0 as usize {
 			let glyph_byte = glyph.buffer[glyph_row * glyph_pitch_abs + glyph_col / 8];
 			let mask = 0b1000_0000 >> glyph_col % 8;
-			let byte_value = if glyph_byte & mask != 0 { 255 } else { 0 };
-			atlas[atlas_row + glyph_row][atlas_col + glyph_col] = byte_value;
+			
+			if glyph_byte & mask != 0 {
+				atlas[atlas_row + spread + glyph_row][atlas_col + spread + glyph_col] = 255;
+			}
 		}
 	}
 
@@ -174,7 +189,7 @@ fn expand_atlas(atlas: &mut Vec<Vec<u8>>, vertical_len: usize, horizontal_len: u
 	}
 }
 
-fn save_to_bitmap(file_path: &str, atlas: &Vec<Vec<u8>>) {
+fn save_to_bitmap(file_path: &str, atlas: &Vec<Vec<i8>>) {
 	let image_width = atlas[0].len();
 	let image_height = atlas.len();
 	let image_row_padding_len = (4 - image_width % 4) % 4;
@@ -245,7 +260,10 @@ fn save_to_bitmap(file_path: &str, atlas: &Vec<Vec<u8>>) {
 	// Pixel data
 	let padding = vec![0u8; image_row_padding_len];
 	for row in atlas.iter().rev() {
-		buffer.extend_from_slice(row);
+		for texel in row {
+			buffer.push((*texel as i32 + 128) as u8);
+		}
+
 		buffer.extend_from_slice(&padding);
 	}
 
@@ -255,6 +273,91 @@ fn save_to_bitmap(file_path: &str, atlas: &Vec<Vec<u8>>) {
 
 	let mut file = fs::File::create(file_path).unwrap();
 	file.write_all(&buffer).unwrap();
+}
+
+fn create_distance_field(atlas: &Vec<Vec<u8>>, spread: f32) -> Vec<Vec<i8>> {
+	let atlas_height = atlas.len();
+	let atlas_width = atlas[0].len();
+
+	let mut field1 = Vec::with_capacity(atlas_height);
+	let mut field2 = Vec::with_capacity(atlas_height);
+
+	for atlas_row in atlas {
+		let mut field1_row = Vec::with_capacity(atlas_width);
+		let mut field2_row = Vec::with_capacity(atlas_width);
+
+		for atlas_texel in atlas_row {
+			if *atlas_texel == 255 {
+				field1_row.push((1000, 1000));
+				field2_row.push((0, 0));
+			}
+			else {
+				field1_row.push((0, 0));
+				field2_row.push((1000, 1000));
+			}
+		}
+
+		field1.push(field1_row);
+		field2.push(field2_row);
+	}
+
+	for row in 0..atlas_height {
+		for col in 0..atlas_width {
+			if row > 0 {
+				compare(&mut field1, row, col, -1, 0);
+				compare(&mut field2, row, col, -1, 0);
+
+				compare(&mut field1, atlas_height - 1 - row, atlas_width - 1 - col, 1, 0);
+				compare(&mut field2, atlas_height - 1 - row, atlas_width - 1 - col, 1, 0);
+			}
+
+			if col > 0 {
+				compare(&mut field1, row, col, 0, -1);
+				compare(&mut field2, row, col, 0, -1);
+
+				compare(&mut field1, atlas_height - 1 - row, atlas_width - 1 - col, 0, 1);
+				compare(&mut field2, atlas_height - 1 - row, atlas_width - 1 - col, 0, 1);
+			}
+		}
+	}
+	
+	let mut final_field = Vec::with_capacity(atlas_height);
+
+	for row in 0..atlas_height {
+		let mut final_field_row = Vec::with_capacity(atlas_width);
+
+		for col in 0..atlas_width {
+			let cell1 = field1[row][col];
+			let dist1 = ((cell1.0 * cell1.0 + cell1.1 * cell1.1) as f32).sqrt();
+
+			let cell2 = field2[row][col];
+			let dist2 = ((cell2.0 * cell2.0 + cell2.1 * cell2.1) as f32).sqrt();
+
+			let dist = (dist1 - dist2).max(-spread).min(spread);
+			let max = if dist.is_sign_positive() { 127.0 } else { 128.0 };
+			let dist_i8 = (dist * max / spread).round() as i8;
+			
+			final_field_row.push(dist_i8);
+		}
+
+		final_field.push(final_field_row);
+	}
+
+	final_field
+}
+
+fn compare(field: &mut Vec<Vec<(u32, u32)>>, pos_row: usize, pos_col: usize, offset_row: isize, offset_col: isize) {
+	let mut other_cell = field[(pos_row as isize + offset_row) as usize][(pos_col as isize + offset_col) as usize];
+	other_cell.0 += offset_row.abs() as u32;
+	other_cell.1 += offset_col.abs() as u32;
+	let other_dist = other_cell.0 + other_cell.1;
+
+	let curr_cell = &mut field[pos_row][pos_col];
+	let curr_dist = curr_cell.0 + curr_cell.1;
+
+	if other_dist < curr_dist {
+		*curr_cell = other_cell;
+	}
 }
 
 fn save_to_font_file(file_path: &str, glyphs: &mut Vec<Glyph>, space_advance: i32, atlas: &Vec<Vec<u8>>) {
