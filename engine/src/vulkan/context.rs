689abfe19ce1e8a7bf6f4477ae00d2cf08804658
Kyle Burke <kyle.n.burke@gmail.com>
2020-11-11 10:24:45 -0600

    Refactor context with code improvements

--- engine/src/vulkan/context.rs
+++ engine/src/vulkan/context.rs
@@ -1,14 +1,11 @@
 use ash::{vk, version::EntryV1_0, version::InstanceV1_0, version::DeviceV1_0, extensions::ext, extensions::khr, vk::Handle};
-use glfw::Context as glfwContext;
+
 use std::{
 	ffi::{CString, CStr},
 	os::raw::{c_void, c_char}
 };
-use super::PhysicalDevice;
 
-const REQUIRED_LAYERS: &[&str] = &["VK_LAYER_KHRONOS_validation"];
-const REQUIRED_INSTANCE_EXTENSIONS: &[&str] = &["VK_EXT_debug_utils"];
-const REQUIRED_DEVICE_EXTENSIONS: &[&str] = &["VK_KHR_swapchain"];
+use super::PhysicalDevice;
 
 pub struct Context {
 	pub instance: ash::Instance,
@@ -33,45 +30,45 @@ pub struct Surface {
 
 impl Context {
 	pub fn new(glfw: &glfw::Glfw, window: &glfw::Window) -> Self {
-		// Create ash entry
+		// Create entry
 		let entry = ash::Entry::new().unwrap();
 
-		// Setup layer and extension strings for foreign function interfacing
-		let layers_c_string: Vec<CString> = REQUIRED_LAYERS.iter().map(|&s| CString::new(s).unwrap()).collect();
-		let layers_ptr_c_char: Vec<*const c_char> = layers_c_string.iter().map(|e| e.as_ptr()).collect();
-
-		let glfw_instance_extensions_string = glfw.get_required_instance_extensions().unwrap();
-		let mut glfw_instance_extensions_c_string: Vec<CString> = glfw_instance_extensions_string.iter().map(|s| CString::new(s.as_str()).unwrap()).collect();
-		let mut instance_extensions_c_string: Vec<CString> = REQUIRED_INSTANCE_EXTENSIONS.iter().map(|&s| CString::new(s).unwrap()).collect();
-		instance_extensions_c_string.append(&mut glfw_instance_extensions_c_string);
+		// Create layer and extension lists
+		let validation_layer = CString::new("VK_LAYER_KHRONOS_validation").unwrap();
+		let required_layers = [validation_layer.as_c_str()];
+		let required_device_extensions = [khr::Swapchain::name()];
 		
-		let device_extensions_c_string: Vec<CString> = REQUIRED_DEVICE_EXTENSIONS.iter().map(|&s| CString::new(s).unwrap()).collect();
-		let device_extensions_ptr_c_char: Vec<*const c_char> = device_extensions_c_string.iter().map(|e| e.as_ptr()).collect();
+		let mut required_instance_extensions = vec![ext::DebugUtils::name()];
+		let required_glfw_instance_extensions_cstring: Vec<CString> = glfw.get_required_instance_extensions().unwrap().iter().map(|s| CString::new(s.as_str()).unwrap()).collect();
+		let required_glfw_instance_extensions_cstr: Vec<&CStr> = required_glfw_instance_extensions_cstring.iter().map(|s| s.as_c_str()).collect();
+		required_instance_extensions.extend_from_slice(&required_glfw_instance_extensions_cstr);
+
+		// Check layer and extension support
+		let available_layers = entry.enumerate_instance_layer_properties().unwrap();
+		for required_layer in &required_layers {
+			available_layers.iter()
+				.find(|available_layer| unsafe { CStr::from_ptr(available_layer.layer_name.as_ptr()) } == *required_layer)
+				.unwrap_or_else(|| panic!("Required layer {} not supported", required_layer.to_str().unwrap()));
+		}
+
+		let available_instance_extensions = entry.enumerate_instance_extension_properties().unwrap();
+		for required_instance_extension in &required_instance_extensions {
+			available_instance_extensions.iter()
+				.find(|available_instance_extension| unsafe { CStr::from_ptr(available_instance_extension.extension_name.as_ptr()) } == *required_instance_extension)
+				.unwrap_or_else(|| panic!("Required instance extension {} not supported", required_instance_extension.to_str().unwrap()));
+		}
 
 		// Create instance
-		let available_layers = entry.enumerate_instance_layer_properties().unwrap();
-		for layer in &layers_c_string {
-			available_layers.iter()
-				.find(|l| unsafe { CStr::from_ptr(l.layer_name.as_ptr()) } == layer.as_c_str())
-				.unwrap_or_else(|| panic!("Required layer {} not supported", layer.to_str().unwrap()));
-		}
-	
-		let available_instance_extensions = entry.enumerate_instance_extension_properties().unwrap();
-		let mut instance_extensions_ptr = Vec::with_capacity(instance_extensions_c_string.len());
-		for instance_extension in &instance_extensions_c_string {
-			available_instance_extensions.iter()
-				.find(|e| unsafe { CStr::from_ptr(e.extension_name.as_ptr()) } == instance_extension.as_c_str())
-				.unwrap_or_else(|| panic!("Required instance extension {} not supported", instance_extension.to_str().unwrap()));
-			instance_extensions_ptr.push(instance_extension.as_ptr());
-		}
-	
 		let engine_name = CString::new("Vulkan Engine").unwrap();
 		let app_info = vk::ApplicationInfo::builder()
 			.engine_name(engine_name.as_c_str())
 			.engine_version(vk::make_version(1, 0, 0))
 			.api_version(vk::make_version(1, 2, 0));
 		
-		let debug_messenger_create_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
+		let layers: Vec<*const c_char> = required_layers.iter().map(|layer| layer.as_ptr()).collect();
+		let instance_extensions: Vec<*const c_char> = required_instance_extensions.iter().map(|extension| extension.as_ptr()).collect();
+		
+		let mut debug_messenger_create_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
 			.message_severity(vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
 				| vk::DebugUtilsMessageSeverityFlagsEXT::ERROR)
 			.message_type(vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
@@ -79,14 +76,12 @@ impl Context {
 				| vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE)
 			.pfn_user_callback(Some(Self::debug_message_callback))
 			.build();
-		
-		let mut debug_messenger_create_info_mut = debug_messenger_create_info;
 	
 		let instance_create_info = vk::InstanceCreateInfo::builder()
 			.application_info(&app_info)
-			.enabled_extension_names(&instance_extensions_ptr)
-			.enabled_layer_names(&layers_ptr_c_char)
-			.push_next(&mut debug_messenger_create_info_mut);
+			.enabled_layer_names(&layers)
+			.enabled_extension_names(&instance_extensions)
+			.push_next(&mut debug_messenger_create_info);
 		
 		let instance = unsafe { entry.create_instance(&instance_create_info, None).unwrap() };
 
@@ -99,19 +94,15 @@ impl Context {
 		};
 
 		// Create surface extension and handle
-		let instance_raw = instance.handle().as_raw();
-		let mut surface_raw: u64 = 0;
-		
-		let result = unsafe { glfw::ffi::glfwCreateWindowSurface(instance_raw as usize, window.window_ptr(), std::ptr::null(), &mut surface_raw as *mut u64) };
-		if result != 0 {
-			panic!("Could not create window surface");
-		}
-
 		let surface_extension = khr::Surface::new(&entry, &instance);
-		let surface_handle = vk::SurfaceKHR::from_raw(surface_raw);
+		let mut surface_handle_raw: u64 = 0;
+		let result = window.create_window_surface(instance.handle().as_raw() as usize, std::ptr::null(), &mut surface_handle_raw as *mut u64);
+		assert_eq!(result, 0, "Could not create window surface");
+		let surface_handle = vk::SurfaceKHR::from_raw(surface_handle_raw);
 
 		// Create the physical device
-		let physical_device = PhysicalDevice::new(&instance, surface_handle, &surface_extension, &device_extensions_c_string);
+		let device_extensions: Vec<CString> = required_device_extensions.iter().map(|extension| CString::new(extension.to_str().unwrap()).unwrap()).collect();
+		let physical_device = PhysicalDevice::new(&instance, surface_handle, &surface_extension, &device_extensions);
 
 		// Create surface format
 		let surface_formats = unsafe { surface_extension.get_physical_device_surface_formats(physical_device.handle, surface_handle).unwrap() };
@@ -121,6 +112,7 @@ impl Context {
 		// Create logical device and queues
 		let graphics_queue_family = physical_device.graphics_queue_family;
 		let present_queue_family = physical_device.present_queue_family;
+
 		let mut device_queue_create_infos = vec![vk::DeviceQueueCreateInfo::builder()
 			.queue_family_index(graphics_queue_family)
 			.queue_priorities(&[1.0])
@@ -134,17 +126,18 @@ impl Context {
 		}
 
 		let features = vk::PhysicalDeviceFeatures::builder();
+		let device_extensions: Vec<*const c_char> = required_device_extensions.iter().map(|extension| extension.as_ptr()).collect();
 
 		let device_create_info = vk::DeviceCreateInfo::builder()
 			.queue_create_infos(&device_queue_create_infos)
 			.enabled_features(&features)
-			.enabled_layer_names(&layers_ptr_c_char)
-			.enabled_extension_names(&device_extensions_ptr_c_char);
+			.enabled_layer_names(&layers)
+			.enabled_extension_names(&device_extensions);
 		
 		let logical_device = unsafe { instance.create_device(physical_device.handle, &device_create_info, None).unwrap() };
 		let graphics_queue = unsafe { logical_device.get_device_queue(graphics_queue_family, 0) };
 		let present_queue = unsafe { logical_device.get_device_queue(present_queue_family, 0) };
-		
+
 		Self {
 			instance,
 			debug_utils,
