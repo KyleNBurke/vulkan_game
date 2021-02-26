1b720373b781ce03034c4ef470c5bff78a959c8a
Kyle Burke <kyle.n.burke@gmail.com>
2021-02-25 22:08:18 -0600

    Add dynamic mesh instancing to example

--- engine/examples/instancing.rs
+++ engine/examples/instancing.rs
@@ -1,5 +1,19 @@
 use utilities::Window;
-use engine::{Renderer, Camera, lights::AmbientLight, scene::Scene, pool::Pool, math::Vector3, Geometry3D, Transform3D, mesh::{StaticInstancedMesh, Material}};
+
+use engine::{Renderer,
+	Camera,
+	lights::AmbientLight,
+	scene::Scene,
+	pool::Pool,
+	math::Vector3,
+	Geometry3D,
+	Transform3D,
+	mesh::{
+		Material,
+		StaticInstancedMesh,
+		InstancedMesh
+	}
+};
 
 fn main() {
 	let mut window = Window::new("Instancing");
@@ -16,25 +30,44 @@ fn main() {
 	let mut scene = Scene::new(camera, ambient_light);
 
 	let mut geometries = Pool::<Geometry3D>::new();
-	let geometry = geometries.add(Geometry3D::create_box());
-	let mut instanced_box = StaticInstancedMesh::new(geometry, Material::Basic);
+	let static_geometry = geometries.add(Geometry3D::create_box());
+	let mut static_instanced_box = StaticInstancedMesh::new(static_geometry, Material::Basic);
 
 	for i in 0..20 {
 		for j in 0..20 {
 			let mut transform = Transform3D::new();
 			transform.position.set((i * 2) as f32, 0.0, (j * 2) as f32);
+			static_instanced_box.transforms.push(transform);
+		}
+	}
+
+	renderer.submit_static_meshes(&geometries, &mut vec![], &mut vec![static_instanced_box]);
+
+	let geometry = scene.geometries.add(Geometry3D::create_box());
+	let mut instanced_box = InstancedMesh::new(geometry, Material::Basic);
+
+	for i in 0..20 {
+		for j in 0..20 {
+			let mut transform = Transform3D::new();
+			transform.position.set((i * 2) as f32, 1.5, (j * 2) as f32);
 			instanced_box.transforms.push(transform);
 		}
 	}
 
-	renderer.submit_static_meshes(&geometries, &mut vec![], &mut vec![instanced_box]);
+	let instanced_box_handle = scene.instanced_meshes.add(instanced_box);
 
 	let mut surface_changed = false;
 
 	window.main_loop(|resized, width, height| {
 		if resized || surface_changed {
 			renderer.handle_resize(width, height);
-			scene.camera.projection_matrix.make_perspective(width as f32 / height as f32, 75.0, 0.1, 10.0);
+			scene.camera.projection_matrix.make_perspective(width as f32 / height as f32, 75.0, 0.1, 50.0);
+		}
+
+		let instanced_box = scene.instanced_meshes.get_mut(&instanced_box_handle).unwrap();
+		for transform in &mut instanced_box.transforms {
+			transform.rotate_y(0.001);
+			transform.update_matrix();
 		}
 
 		surface_changed = renderer.render(&mut scene);
